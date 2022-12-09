4d186a20cbc92f297b538cce8ecfc163a94a0f47
Hicham Meftah <hmeftah@c2r2p5.1337.ma>
2022-12-09 19:19:08 +0100

    Finishing things off

--- client_bonus.c
+++ client_bonus.c
@@ -6,13 +6,13 @@
 /*   By: hmeftah <hmeftah@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 16:43:22 by hmeftah           #+#    #+#             */
-/*   Updated: 2022/12/09 16:36:55 by hmeftah          ###   ########.fr       */
+/*   Updated: 2022/12/09 19:15:17 by hmeftah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-t_data g_server_data;
+t_data	g_server_data;
 
 static void	encoder(int pid, char *string)
 {
@@ -61,8 +61,8 @@ int	main(int argc, char **argv)
 	}
 	encoder(ft_atoi(argv[1]), argv[2]);
 	if (g_server_data.data_recieved == g_server_data.data_sent)
-		ft_printf("Message has been sent successfully");
+		ft_printf("[✓]Info: The Message has been sent successfully\n");
 	else
-		ft_printf("Error: Something lost in transmission");
+		ft_printf("[☓]Error: Transmition got caught off\n");
 	return (0);
 }
